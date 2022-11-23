cc299c0496acbcddd853d14fd780efde22d393c7
unknown <md.rafiur.rahman.saikat@gmail.com>
2022-11-23 23:38:51 +0600

    Initial Setup Done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEnNbsmhSz8_bMv0vFv3v2mYO4VJpr93E",
+  authDomain: "dream-b-b903f.firebaseapp.com",
+  projectId: "dream-b-b903f",
+  storageBucket: "dream-b-b903f.appspot.com",
+  messagingSenderId: "577550243900",
+  appId: "1:577550243900:web:decb7d5943128b6c8acb39"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
